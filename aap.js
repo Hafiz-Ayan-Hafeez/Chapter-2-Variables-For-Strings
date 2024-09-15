9a3ab6a8afdcc2cfb781a8e1786dcb3c4c25d1a7
Hafiz-Ayan-Hafeez <hafiz.ayan.public@gmail.com>
2024-09-15 22:04:41 +0500

    Chapter-2 Done

---
+++ aap.js
@@ -0,0 +1,27 @@
+var username = "Ayan";
+
+var myName = "Hafiz Ayan Bin Hafeez";
+
+var message = "Hello World";
+alert(message);
+
+var username = "Ayan";
+var age = "15";
+var bio = "Certified Mobile Application Development";
+alert(username);
+alert(age);
+alert(bio);
+
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+var email = "example@example.com";
+alert("My Email Address Is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+var h2 = document.querySelector("h2");
+h2.innerHTML = "Yah! I can write HTML content through JavaScript";
+
+var string =  "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+alert(string);
